ef2fca0d4135579651b7da9b0985da491a2025b5
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-09-30 20:40:20 +0300

    Read the file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 // Sort coins by Market Value
+#include <iostream>
+#include <fstream>
+#include <string>
+
+const int NUMBER_OF_ELEMENTS = 100;
 
 int main() {
+  std::string coins[NUMBER_OF_ELEMENTS];
+
+  std::ifstream file("coins.data");
+
+  if (!file) {
+    std::cout << "Unable to open file." << std::endl;
+    return 1;
+  }
+
+  int lineCount = 0;
+  std::string line;
+
+  while (std::getline(file, line)) {
+    if (lineCount < NUMBER_OF_ELEMENTS) {
+      coins[lineCount] = line;
+      lineCount++;
+    } else {
+      break;
+    }
+  }
+
+  file.close();
+
   return 0;
 }
