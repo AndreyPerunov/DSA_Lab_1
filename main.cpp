c0d63af508b41b9754f29c8cca24cdad04efc499
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-10-01 21:29:08 +0300

    Made data visualization

--- main.cpp
+++ main.cpp
@@ -3,11 +3,34 @@
 #include <string>
 #include <chrono>
 
-const int NUMBER_OF_ELEMENTS = 9999;
+const int NUMBER_OF_ELEMENTS = 10;
 
-void print(int array[]) {
+void printSelectionSort(int array[], int green, int red, int yellow) {
   for (int i = 0; i < NUMBER_OF_ELEMENTS; ++i) {
-    std::cout << "  " << array[i] << "; ";
+    if (i == green) {
+      std::cout << "\033[1;32m " << array[i] << "\033[0m; ";
+    } else if (i == red) {
+      std::cout << "\033[1;31m " << array[i] << "\033[0m; ";
+    } else if (i == yellow) {
+      std::cout << "\033[1;33m " << array[i] << "\033[0m; ";
+    } else {
+      std::cout << "  " << array[i] << "; ";
+    }
+  }
+  std::cout<<std::endl;
+}
+
+void printQuickSort(int array[], int green, int red, int blue) {
+  for (int i = 0; i < NUMBER_OF_ELEMENTS; ++i) {
+    if (i == green) {
+      std::cout << "\033[1;32m " << array[i] << "\033[0m; ";
+    } else if (i == red) {
+      std::cout << "\033[1;31m " << array[i] << "\033[0m; ";
+    } else if (i == blue) {
+      std::cout << "\033[1;34m " << array[i] << "\033[0m; ";
+    } else {
+      std::cout << "  " << array[i] << "; ";
+    }
   }
   std::cout<<std::endl;
 }
@@ -16,6 +39,7 @@ int findSmallest(int array[], int skip, int &comparisons) {
   int smallestIndex = skip;
   for (int i = skip + 1; i < NUMBER_OF_ELEMENTS; ++i) {
     comparisons++;
+    printSelectionSort(array, skip - 1, i, smallestIndex);
     if (array[i] < array[smallestIndex]) {
       smallestIndex = i;
     }
@@ -52,7 +76,7 @@ void swap(int &a, int &b) {
   b = temp;
 }
 
-void insertionSort(int array[], int &transpositions, int &comparisons){
+void selectionSort(int array[], int &transpositions, int &comparisons){
   for (int i = 0; i < NUMBER_OF_ELEMENTS; i++) {
     swap(array[i], array[findSmallest(array, i, comparisons)]);
     transpositions++;
@@ -65,6 +89,7 @@ int partition(int array[], int low, int high, int &transpositions, int &comparis
 
   for (int i = low + 1; i <= high; i++) {
     comparisons++;
+    printQuickSort(array, low, i, leftwall);
     if (array[i] < pivot) {
       leftwall++;
       swap(array[i], array[leftwall]);
@@ -73,6 +98,7 @@ int partition(int array[], int low, int high, int &transpositions, int &comparis
   }
 
   swap(array[low], array[leftwall]);
+  printQuickSort(array, leftwall, 99999999, 99999999);
   transpositions++;
   return leftwall;
 }
@@ -89,33 +115,38 @@ void quickSort(int array[], int low, int high, int &transpositions, int &compari
 int main() {
   int coins1[NUMBER_OF_ELEMENTS] = {0};
   int coins2[NUMBER_OF_ELEMENTS] = {0};
-  int transpositions;
-  int comparisons;
+  int transpositions1 = 0;
+  int comparisons1 = 0;
+  int transpositions2 = 0;
+  int comparisons2 = 0;
   readData("coins.txt", coins1);
   readData("coins.txt", coins2);
   
   std::cout << std::endl << "Insertions Sort: " << std::endl;
-  transpositions = 0;
-  comparisons = 0;
   auto start1 = std::chrono::high_resolution_clock::now();
-  insertionSort(coins1, transpositions, comparisons);
+  selectionSort(coins1, transpositions1, comparisons1);
   auto end1 = std::chrono::high_resolution_clock::now();
-  std::cout << "Number of Transpositions: " << transpositions << std::endl;
-  std::cout << "Number of Comparisons: " << comparisons << std::endl;
   std::chrono::duration<float> duration1 = end1 - start1;
-  std::cout << "Time: " << duration1.count()*1000 << "ms" << std::endl << std::endl;
-  
-  
-  std::cout << "Quick Sort: " << std::endl;
-  transpositions = 0;
-  comparisons = 0;
+
+  std::cout << std::endl << "Quick Sort: " << std::endl;
   auto start2 = std::chrono::high_resolution_clock::now();
-  quickSort(coins2, 0, NUMBER_OF_ELEMENTS - 1, transpositions, comparisons);
+  quickSort(coins2, 0, NUMBER_OF_ELEMENTS - 1, transpositions2, comparisons2);
   auto end2 = std::chrono::high_resolution_clock::now();
-  std::cout << "Number of Transpositions: " << transpositions << std::endl;
-  std::cout << "Number of Comparisons: " << comparisons << std::endl;
   std::chrono::duration<float> duration2 = end2 - start2;
-  std::cout << "Time: " << duration2.count()*1000 << "ms" << std::endl << std::endl;
+
+
+  std::cout << std::endl << "Insertions Sort Benchmark: " << std::endl;
+  std::cout << "  Number of Transpositions: " << transpositions1 << std::endl;
+  std::cout << "  Number of Comparisons: " << comparisons1 << std::endl;
+  std::cout << "  Time: " << duration1.count()*1000 << "ms" << std::endl << std::endl;
+  
+  std::cout << "Quick Sort Benchmark: " << std::endl;
+  std::cout << "  Number of Transpositions: " << transpositions2 << std::endl;
+  std::cout << "  Number of Comparisons: " << comparisons2 << std::endl;
+  std::cout << "  Time: " << duration2.count()*1000 << "ms" << std::endl << std::endl;
+
+
+  std::cin.get();
 
   return 0;
 }
