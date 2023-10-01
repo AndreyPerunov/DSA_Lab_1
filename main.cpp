ca2b196315b273d275e91a225f572eb7cdb64d0c
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-10-01 14:06:30 +0300

    Made quick sort

--- main.cpp
+++ main.cpp
@@ -23,32 +23,32 @@ int findSmallest(int array[], int skip) {
 }
 
 void readData(std::string filename, int arr[]) {
-    std::ifstream file(filename);
+  std::ifstream file(filename);
 
-    if (!file.is_open()) {
-        std::cerr << "Error opening file" << std::endl;
-        return;
+  if (!file.is_open()) {
+    std::cerr << "Error opening file" << std::endl;
+    return;
+  }
+
+  std::string line;
+  int count = 0;
+
+  while (count < NUMBER_OF_ELEMENTS && getline(file, line)) {
+    try {
+      arr[count] = std::stoi(line);
+      count++;
+    } catch (const std::invalid_argument& e) {
+      std::cerr << "Error reading line number " << count + 1 << std::endl << std::endl;
     }
+  }
 
-    std::string line;
-    int count = 0;
-
-     while (count < NUMBER_OF_ELEMENTS && getline(file, line)) {
-        try {
-            arr[count] = std::stoi(line);
-            count++;
-        } catch (const std::invalid_argument& e) {
-            std::cerr << "Error reading line number " << count + 1 << std::endl << std::endl;
-        }
-    }
-
-    file.close();
+  file.close();
 }
 
 void swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
+  int temp = a;
+  a = b;
+  b = temp;
 }
 
 void insertionSort(int array[]){
@@ -57,9 +57,31 @@ void insertionSort(int array[]){
   }
 }
 
+int partition(int array[], int low, int high) {
+  int pivot = array[low];
+  int leftwall = low;
+
+  for (int i = low + 1; i <= high; i++) {
+    if (array[i] < pivot) {
+      leftwall++;
+      swap(array[i], array[leftwall]);
+    }
+  }
+
+  swap(array[low], array[leftwall]);
+  return leftwall;
+}
+
+void quickSort(int array[], int low, int high){
+  if (low < high) {
+    int pivotLocation = partition(array, low, high);
+    quickSort(array, low, pivotLocation);
+    quickSort(array, pivotLocation + 1, high);
+  }
+}
+
 int main() {
   int coins[NUMBER_OF_ELEMENTS] = {0};
   readData("coins.txt", coins);
-  insertionSort(coins);
   return 0;
 }
